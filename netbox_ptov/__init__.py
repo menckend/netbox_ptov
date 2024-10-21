d80f4a673c0ff65a36d081da8673bcf1f1dc7ccf
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-10-21 14:55:17 -0400

    cleanup

--- netbox_ptov/__init__.py
+++ netbox_ptov/__init__.py
@@ -1,15 +1,14 @@
-"""Top-level package for NetBox ptov."""
-
-# __author__ = """Mencken Davidson"""
-# __email__ = "mencken@gmail.com"
-# __version__ = "0.1.14"
-
-
 from netbox.plugins import PluginConfig
 from .version import __version__
 
 
 class ptovConfig(PluginConfig):
+    name = 'netbox_ptov'
+    verbose_name = 'netbox_ptov'
+    description = 'Does this description take preference, or some other one?'
+    version = __version__
+    author = 'Mencken the init.py Davidson'
+    author_email = 'mencken@gmail.com.gmail.mencken.com'
     base_url = "netbox_ptov"
     default_settings = {
         'device_ext_page': 'right',
