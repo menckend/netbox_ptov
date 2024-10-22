138d59dab73a83419b5a3cb31ed23e73ea78b59b
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-10-22 18:44:50 -0400

    housekeeping

--- docs/conf.py
+++ docs/conf.py
@@ -82,3 +82,22 @@ def prepare_jinja_env(jinja_env) -> None:
     jinja_env.tests["contains"] = contains
 
 autoapi_prepare_jinja_env = prepare_jinja_env
+
+
+html_theme_options = {
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    'flyout_display': 'hidden',
+    'version_selector': True,
+    'language_selector': True,
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': -1,
+    'includehidden': True,
+    'titles_only': False
+}
