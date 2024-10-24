c68428efb36bab67cd566f127d18621b0d09ce43
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-10-24 15:58:57 -0400

    Update __init__.py

--- netbox_ptov/__init__.py
+++ netbox_ptov/__init__.py
@@ -8,8 +8,8 @@ from netbox_ptov._version import __version__
 class ptov_config(PluginConfig):
     name = 'netbox_ptov'
     verbose_name = 'Physical to Virtual-lab'
-    description = 'Builds GNS3 labs w/ config and topology scraped from Arista switches in device tabls'
-    version = __version__
+    description = 'Builds GNS3 labs with config and topology scraped from Arista switches in device tabls'
+    version = 0.1
     author = 'Mencken Davidson'
     author_email = 'mencken@gmail.com'
     base_url = 'netbox_ptov'
