489e951565f0477f812a814956b554f467d3bb18
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-10-25 09:40:09 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ html_static_path = ['_static']
 
 # -- Extensions to use ---------------------------------------------------
 extensions = [
-    'autoapi.extension'
+    'autoapi.extension',
     'myst_parser',
     'sphinx.ext.autodoc',  # Core Sphinx library for auto html doc generation from docstrings
     'sphinx.ext.autodoc.typehints',
