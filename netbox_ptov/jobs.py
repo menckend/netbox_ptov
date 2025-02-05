c854c7bb8227161dfca4127b41b09ca29ff237d6
menckend <mencken@gmail.com>
2025-02-05 12:12:21 -0500

    more job wrangling

--- netbox_ptov/jobs.py
+++ netbox_ptov/jobs.py
@@ -9,6 +9,70 @@ class PToVJob(JobRunner):
     """
     class Meta:
         name = "Create Virtual Lab"
+
+    def run(self, request, username, password, switchlist, servername, prjname, **kwargs):
+        obj = self.job.object
+        # your logic goes here
+        self.request = request
+        self.username = username
+        self.password = password
+        self.switchlist = switchlist
+        self.servername = servername
+        self.prjname = prjname
+
+        class MessagesHandler(logging.Handler):
+            def __init__(self, job, request):
+#                super().__init__()
+                self.job = job
+                self.request = request
+
+            def emit(self, record):
+                msg = self.format(record)
+                # Log to job
+                self.job.log_info(msg)
+                # Forward to Django messages if request exists
+                if self.request:
+                    messages.info(self.request, msg)
+
+        # Set up logging
+        logger = logging.getLogger('ptovnetlab')
+        handler = MessagesHandler(self, self.request)
+        handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        logger.addHandler(handler)
+
+        try:
+            # Execute p_to_v with provided parameters
+            result = ptvnl.p_to_v(
+                username=self.username,
+                passwd=self.password,
+                servername=self.servername,
+                switchlist=self.switchlist,
+                prjname=self.prjname
+            )
+
+            # Log success
+            self.log_success(f"Project Created: {self.prjname} on {self.servername}")
+            if result:
+                self.log_info(f"Project URL: {result}")
+
+            return result
+
+        except Exception as e:
+            # Log error and re-raise
+            self.log_failure(str(e))
+            raise
+
+        finally:
+            # Clean up logging
+            logger.removeHandler(handler)
+
+
+class PToVJob2Bad(JobRunner):
+    """
+    Job for executing p_to_v function in the background.
+    """
+    class Meta:
+        name = "Create Virtual Lab"
         # description = "Creates a virtual lab in GNS3 from NetBox devices"
 
 
