1e1a6e30a2d365f045059cc9867449f34cc088f4
menckend <mencken@gmail.com>
2025-02-06 15:32:12 -0500

    cleaner

--- netbox_ptov/views.py
+++ netbox_ptov/views.py
@@ -60,7 +60,8 @@ def golab(request: forms.golabForm) -> django.http.HttpResponse:
                 #result_out = str(ptvnl.p_to_v(username=username, passwd=password , servername=servername, switchlist=switchlist, prjname=projectname))
                 #result_out = 'dummy run'
                 messages.info(request, f'Completing your request as a background job.', extra_tags='safe')
-                ptovJob.enqueue_once(instance=nvgb.BaseObjectView.get_object(form.cleaned_data['serverselect_in'].pk), username=username, password=password, switchlist=switchlist, servername=servername, projectname=projectname)
+                gns3serverid = form.cleaned_data['serverselect_in'].pk
+                ptovJob.enqueue_once(instance=gns3srv.objects.get(pk=gns3serverid), username=username, password=password, switchlist=switchlist, servername=servername, projectname=projectname)
                 #return super().save(*args, **kwargs)
             except Exception as e:
                 # Handle any exceptions and add an error message
