dcf126d113633099506055571717e0109168a5f0
menckend <mencken@gmail.com>
2025-02-06 22:44:05 -0500

    asd

--- netbox_ptov/models.py
+++ netbox_ptov/models.py
@@ -63,3 +63,43 @@ class switchtojob(NetBoxModel):
 	to='dcim.device',
         on_delete=models.CASCADE,
     )
+
+class ptovjob(NetBoxModel):
+    """
+    A class to represent the Django model virtual-labs requested through the netbox_ptov plugin
+
+    ...
+
+    Attributes
+    ----------
+    name
+    gns3srv : str
+    gns3prjname : str
+    gns3prjid : str
+    eosuname : str
+    eospasswd : str
+    """
+
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("plugins:netbox_ptov:ptovjob", args=[self.pk])
+
+    gns3srv = models.ForeignKey(
+        to=gns3srv,
+        on_delete=models.CASCADE,
+    )
+
+    gns3prjname = models.CharField(max_length=100)
+
+    gns3prjid = models.CharField(max_length=200)
+
+    eosuname = models.CharField(max_length=100)
+
+    eospasswd = models.CharField(max_length=100)
