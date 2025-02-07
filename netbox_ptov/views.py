63073a3c7387e976084fab9549cf5b493633a0e7
menckend <mencken@gmail.com>
2025-02-07 13:21:29 -0500

    asdf

--- netbox_ptov/views.py
+++ netbox_ptov/views.py
@@ -55,8 +55,8 @@ def golab(request: forms.golabForm) -> django.http.HttpResponse:
                 # Call the function that does all of the work
                 messages.info(request, f'Completing your request as a background job.', extra_tags='safe')
                 serverobject = get_object_or_404(GNS3Server, pk=form.cleaned_data['serverselect_in'].pk)                
-                print(type(serverobject))
-                print(serverobject.pkr)
+                print(serverobject.self)
+                print(serverobject.pk)
                 print(serverobject.name)
                 
                 ptovJob.enqueue_once(
