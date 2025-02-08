2ba61e1d91b1610d92dee5f31830a930b5b89c5c
menckend <mencken@gmail.com>
2025-02-08 17:08:08 -0500

    closer still

--- netbox_ptov/views.py
+++ netbox_ptov/views.py
@@ -64,14 +64,14 @@ def golab(request: forms.golabForm) -> django.http.HttpResponse:
                 #messages.info(request, ptovobject.name)
                 ptovjob_new = ptovJob.enqueue(
                     immediate = True,
-                    interval=None,
-                    'name' = 'Virt-lab job',
-                    'job' = 'P to V lab',
-                    'username' = username,
-                    'password' = password,
-                    'switchlist' = switchlist,
-                    'servername' = servername,
-                    'projectname' = projectname
+                    interval = None,
+                    name = 'Virt-lab job',
+                    job = 'P to V lab',
+                    username = username,
+                    password = password,
+                    switchlist = switchlist,
+                    servername = servername,
+                    projectname = projectname
                 )
                 joburl = ptovjob_new.get_absolute_url()
                 messages.info(request, f'Job has been enqueued as: ' + joburl)
