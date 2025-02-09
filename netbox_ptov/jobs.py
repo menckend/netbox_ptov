566297a7d69498d5de4efc5669784e38e65b1c8f
menckend <mencken@gmail.com>
2025-02-09 12:13:48 -0500

    adqwe

--- netbox_ptov/jobs.py
+++ netbox_ptov/jobs.py
@@ -40,21 +40,21 @@ class ptovJob(JobRunner):
                 self.job = job
 
             def emit(self, record):
-                log_entry = self.format(record)
+                log_entry = self.format(str(record))
                 if not self.job.data:
                     self.job.data = []
-                self.job.data.append(log_entry)
+                self.job.data.append(str(log_entry))
                 self.job.save()  # Save the updated job data
 
         class CustomFormatter(logging.Formatter):
             def formatTime(self, record, datefmt=None):
                 # Ensure record.created is a float timestamp
-                timestamp = self.converter(record.created)
+                #timestamp = self.converter(record.created)
                 messages.info(request, f'timestamp: {str(timestamp)}', extra_tags='safe')
-                dt_object = datetime.datetime.fromtimestamp(str(timestamp), tz=timezone.utc)
-                messages.info(request, f'dt_object: {str(dt_object)}', extra_tags='safe')
-                return dt_object.isoformat()
-                #return dt_object
+                #dt_object = datetime.datetime.fromtimestamp(str(timestamp), tz=timezone.utc)
+                #messages.info(request, f'dt_object: {str(dt_object)}', extra_tags='safe')
+                #return dt_object.isoformat()
+                return record.created
 
         logging.basicConfig(level=logging.INFO,
             format='%(asctime)s - %(levelname)s - %(message)s',
