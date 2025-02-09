4589b9b1aa3b3ff25868b469a213e60b2b3c072f
menckend <mencken@gmail.com>
2025-02-08 20:31:41 -0500

    sprank

--- netbox_ptov/jobs.py
+++ netbox_ptov/jobs.py
@@ -10,17 +10,17 @@ from django.shortcuts import render, redirect
 import json
 
 
-class MessagesHandler(logging.Handler):
-    def __init__(self, request, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.request = request
+#class MessagesHandler(logging.Handler):
+#    def __init__(self, request, *args, **kwargs):
+#        super().__init__(*args, **kwargs)
+#        self.request = request
 
-    def emit(self, record):
-        try:
-            msg = self.format(record)
-            messages.info(self.request, msg)
-        except Exception:
-            self.handleError(record)
+#    def emit(self, record):
+#        try:
+#            msg = self.format(record)
+#            messages.info(self.request, msg)
+#        except Exception:
+#            self.handleError(record)
 
 
 class ptovJob(JobRunner):
@@ -40,14 +40,14 @@ class ptovJob(JobRunner):
         obj = self.job.object
 
         # Create a custom logging handler
-        messages_handler = MessagesHandler(self)
-        messages_handler.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        messages_handler.setFormatter(formatter)
+ #       messages_handler = MessagesHandler(self)
+ #       messages_handler.setLevel(logging.INFO)
+ #       formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ #       messages_handler.setFormatter(formatter)
 
         # Get the logger used by ptovnetlab.p_to_v
-        logger = logging.getLogger('ptovnetlab')
-        logger.addHandler(messages_handler)
+ #       logger = logging.getLogger('ptovnetlab')
+ #       logger.addHandler(messages_handler)
 
         try:
             # Call the function that does all of the work
